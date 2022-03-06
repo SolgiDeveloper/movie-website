7d7c3829ba7930c2fbdf007b24b25cca144aac12
Mohammad Solgi <m.solgi@apasaico.local>
2022-03-07 01:37:07 +0330

    add img and config nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/defaults.scss'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -26,7 +28,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
---
+++ pages/movies/_movieId.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "_movieId"
+}
+</script>
+
+<style scoped>
+
+</style>
